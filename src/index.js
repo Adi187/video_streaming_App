72c364dc7079e0051a400e122e666f47a6ea9033
Aditya sharma <adityaraysharma@gmail.com>
2024-05-06 19:48:25 +0530

    added utils for api handling

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//require('dotenv').config({path:'/env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:'./env'
+})
+connectDB().then(()=>{
+    app.listen(process.env.port||8000,()=>{
+        console.log(`Server started on port: ${process.env.port||8000}`);
+    });
+}).catch((err)=>{
+    console.log("MONGODB CONNECTION ERROR: ",err);
+})
+
+/*
+import express from "express";
+(async ()=>{
+    try{
+        mongoose.connect(`${process.env.MONGODB_URL}/{DB_NAME}`)
+        app.on("error",()=>{
+            console.log("ERR:",error);
+            throw error
+        })
+
+    app.listen(process.env.PORT,()=>{
+        console.log("Listening on port: ",process.env.PORT)
+    })
+
+    }catch(error){
+        console.error("ERROR: ",error)
+        throw error
+    }
+})();
+*/
